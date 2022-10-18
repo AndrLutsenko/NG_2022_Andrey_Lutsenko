2587c99c538eec01956d31e42b857bdd1e1f2c39
Olivkerok <116101572+Olivkerok@users.noreply.github.com>
2022-10-18 20:22:14 +0300

    Create task2.py

---
+++ Lesson_1/task2.py
@@ -0,0 +1,32 @@
+import math
+while True:
+    print('Value A')
+    a=float(input())
+    print('Value B')
+    b=float(input())
+    print('Enter an operation +,-,*,/,^,√')
+    operation=str(input())
+    if operation=='+':
+        c=a+b
+        print(c)
+    if operation=='-':
+        c=a-b
+        print(c)
+    if operation=='*':
+        c=a*b
+        print(c)
+    if operation=='/':
+        c=a/b
+        print(c)
+    if operation=='^':
+        c=a*a
+        d=b*b
+        print('A^2='+str(c), 'B^2='+str(d))
+    if operation=='√':
+        c=math.sqrt(a)
+        d=math.sqrt(b)
+        print('√A='+str(c), '√B='+str(d))
+    if operation=='V':
+        print('🧡🧡🧡🧡🧡🧡🧡🧡🧡🧡')
+        print('🧡  No "V", only "√"  🧡')
+        print('🧡🧡🧡🧡🧡🧡🧡🧡🧡🧡')
