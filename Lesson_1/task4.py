af5b9e0c7048751dd5e5bbda7b0b7c6f370193e9
Olivkerok <116101572+Olivkerok@users.noreply.github.com>
2022-10-20 14:52:54 +0300

    Create task4.py

---
+++ Lesson_1/task4.py
@@ -0,0 +1,26 @@
+import math
+print('Value a')
+value_a=int(input()) 
+print('Value b')
+value_b=int(input()) 
+print('Value c')
+value_c=int(input()) 
+if value_b>0 and value_c>0:
+    print(f'Your equation looks like {value_a}x^2+{value_b}x+{value_c}=0')
+if value_b<0 and value_c>0:
+    print(f'Your equation looks like {value_a}x^2{value_b}x+{value_c}=0')
+if value_b>0 and value_c<0:
+    print(f'Your equation looks like {value_a}x^2+{value_b}x{value_c}=0')
+if value_b<0 and value_c<0:
+    print(f'Your equation looks like {value_a}x^2{value_b}x{value_c}=0')
+discriminant=value_b*value_b-(4*value_a*value_c)
+print('Discriminant= ',discriminant)
+if discriminant>0:
+    x1=(-value_b+math.sqrt(discriminant))/(2*value_a)
+    x2=(-value_b-math.sqrt(discriminant))/(2*value_a)
+    print(f'x1={x1} x2={x2}')
+if discriminant==0:
+    x0=(-value_b)/(2*value_a)
+    print(f'x={x0}')
+if discriminant<0:
+    print(f'There are no roots')
