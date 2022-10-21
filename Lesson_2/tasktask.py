eb2af8500fb781c0ad20debd4d68d742ac334917
Olivkerok <lutsenko03andr@gmail.com>
2022-10-21 17:56:23 +0300

    Added files

---
+++ Lesson_2/tasktask.py
@@ -0,0 +1,10 @@
+import turtle
+turtle.forward(100)
+turtle.left(90)
+turtle.forward(50)
+turtle.left(90)
+turtle.forward(100)
+turtle.left(90)
+turtle.forward(50)
+turtle.left(90)
+# https://opentechschool.github.io/python-beginners/ru/simple_drawing.html
