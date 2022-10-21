fa4c5027bbc0aad27d0d99d217d3affe6b8ce0a8
Olivkerok <116101572+Olivkerok@users.noreply.github.com>
2022-10-21 11:21:32 +0300

    Update task4.py

--- Lesson_1/task4.py
+++ Lesson_1/task4.py
@@ -5,19 +5,11 @@ print('Value b')
 value_b=int(input()) 
 print('Value c')
 value_c=int(input()) 
-if value_b>0 and value_c>0:
-    print(f'Your equation looks like {value_a}x²+{value_b}x+{value_c}=0')
-if value_b<0 and value_c>0:
-    print(f'Your equation looks like {value_a}x²{value_b}x+{value_c}=0')
-if value_b>0 and value_c<0:
-    print(f'Your equation looks like {value_a}x²+{value_b}x{value_c}=0')
-if value_b<0 and value_c<0:
-    print(f'Your equation looks like {value_a}x²{value_b}x{value_c}=0')
 discriminant=value_b*value_b-(4*value_a*value_c)
 print('Discriminant= ',discriminant)
 if discriminant>0:
     x1=(-value_b+math.sqrt(discriminant))/(2*value_a)
-    x2=(-value_b-math.sqrt(discriminant))/(2*value_a)
+    x2=(-value_b-math.sqrt(discriminant))/(2*vale_a)
     print(f'x1={x1} x2={x2}')
 if discriminant==0:
     x0=(-value_b)/(2*value_a)
