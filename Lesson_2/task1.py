63a668cf965dcf593fefd1eab1da9ab28b42fd14
Olivkerok <116101572+Olivkerok@users.noreply.github.com>
2022-10-21 16:16:31 +0300

    Delete Lesson_2 directory

--- Lesson_2/task1.py
+++
@@ -1,9 +0,0 @@
-#Алгоритмы и структуры данных Лабораторна робота 1 -> Задание 2
-import random
-x=int(input("Введіть кількість елементів масиву"))
-n=[random.randint(1,50) for i in range(x)]
-print(n)
-c=1
-for i in range(x):
-    c*=n[i]
-    print(c)
