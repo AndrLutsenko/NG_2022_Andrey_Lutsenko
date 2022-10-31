ec34d81bde516092ce4f32cd07687cfd2df671a0
Olivkerok <lutsenko03andr@gmail.com>
2022-10-31 18:56:34 +0200

    Change file

--- Lesson_3/task2.py
+++ Lesson_3/task2.py
@@ -1,25 +1,32 @@
-def enterString():
-    enter_string = input("Enter string: ")
+def enterString(text):
+    enter_string = str(input(text))
     return enter_string
-def menu():
-    section=str(input("Menu: \n1. String sort\n2. Quantity of elements\n3. Vowels and consonants\n4. Partition and reverse\n5. World by number\n6. Enter string again\n7. Exit\nChoose a section:\n"))
-    #if section=="1":
-        #def SortString():
-    #if section=="2":
-        #def QuantityElements():
-    #if section=="3":
-        #def VowelsAndConsonants():
-    #if section=="4":
-        #def PartitionAndReverse():
-    #if section=="5":
-        #def WorldByNumber():
-    if section=="6":
-        def EnterStringRe():
-            enter_string = input()
-            return enter_string
-        EnterStringRe()
-    if section=="7":
-        def exit():
-            exit
-enterString()
-menu()
+
+def sortString(sorts):
+    sort1=list(sorts)
+    sort1.sort()
+    return sort1
+def resultsort():
+    print ("Result: " + str(sortString(enterString("Your String: "))))
+
+section=str(input("Menu: \n1. String sort\n2. Quantity of elements\n3. Vowels and consonants\n4. Partition and reverse\n5. World by number\n6. Enter string again\n7. Exit\nChoose a section:\n"))
+
+if section=='1':
+    resultsort()
+
+#if section=="2":
+    #def QuantityElements():
+#if section=="3":
+    #def VowelsAndConsonants():
+#if section=="4":
+    #def PartitionAndReverse():
+#if section=="5":
+    #def WorldByNumber():
+if section=="6":
+    def EnterStringRe():
+        enter_string = input()
+        return enter_string
+    EnterStringRe()
+if section=="7":
+    def exit():
+        exit
