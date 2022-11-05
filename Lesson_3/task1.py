4cc6b07583694eef235eecc8a8fe9097f37c3189
Olivkerok <lutsenko03andr@gmail.com>
2022-11-06 00:09:17 +0200

    Change file

--- Lesson_3/task1.py
+++ Lesson_3/task1.py
@@ -1,29 +1,25 @@
 import math
+
 def EnterValue(text):
     enter_value = int(input(text))
     return enter_value
 
-def operation(first,second):
-    operation=str(input("Enter an operation +,-,*,/,^,√: "))
-    if operation=="+":
-        return first+second
-
-    if operation=="-":
-        return first-second
-
-    if operation=="*":
-        return first*second
-    
-    if operation=="/":
-        return first/second
-
-    if operation=="^":
-        return first**second
-
-    if operation=="√":
-        return "First element sqrt= " + str(math.sqrt(first)), "Second element sqrt= " + str(math.sqrt(second))
+def operation(first, second, code):
+    match code:
+        case 1:
+            return first + second
+        case 2:
+            return first - second
+        case 3:
+            return first * second
+        case 4:
+            return first/second
+        case 5:
+            return first**second
+        case 6:
+            return "SQRT first element= " + str(math.sqrt(first)), "SQRT second element= " + str(math.sqrt(second))
 
 def result():
-    print ("Result: " + str(operation(EnterValue("Enter first value: "),EnterValue("Enter second value: "))))
+    print ("Result: " + str(operation(EnterValue("Enter first value: "),EnterValue("Enter second value: "),EnterValue("Enter operation:\n1.Sum(+)\n2.Subtraction(-)\n3.Multiplication(*)\n4.Division(/)\n5.Power(^)\n6.Root(√)\n"))))
 
 result()
