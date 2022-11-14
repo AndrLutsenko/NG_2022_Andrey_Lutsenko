d803437072d3cd8005ce71d61f7e9ccd73cf27c5
Olivkerok <lutsenko03andr@gmail.com>
2022-11-14 23:21:07 +0200

    Change file

--- Lesson_2/task1.py
+++ Lesson_2/task1.py
@@ -1,17 +1,5 @@
-dicts=input(dict())#"Enter the string: ")
-print(f'Your string: ', dicts)
-#Exercise 1
-s_string = set(dicts)
-print(s_string)
-s_string = set(dicts) #non-repeating elements
-summ = [] #Summation of elements
-#Exercise 2
-for s_symbol in s_string:
-    count = 0
-    for symbol in dicts:
-        if s_symbol == symbol:
-            count += 1
-    summ.append(f'{s_symbol}= {count}')
-    summ.sort()
-for item in summ:
-    print(item)
+import pandas as pd
+
+strings = str(input("Enter string: "))
+list_string=list(strings)
+print(pd.value_counts(list_string))
