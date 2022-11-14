f6bd527e1b003e5a7ccec66620dccb7327ef520c
Olivkerok <lutsenko03andr@gmail.com>
2022-11-14 23:26:17 +0200

    Change file

--- Lesson_3/task3.py
+++ Lesson_3/task3.py
@@ -1,4 +1,15 @@
-from collections import Counter
-etner_string=str(input("Etner string: "))
-result=Counter(etner_string)
-print("\n",result)
+import pandas as pd
+
+def enterstring():
+    strings = str(input("Enter string: "))
+    list_string=list(strings)
+    return list_string
+
+def operation(list_string):
+    result=pd.value_counts(list_string)
+    return result
+
+def result():
+    print(operation(enterstring()))
+
+result()
